26949682b9f5d834feb7fc3de24b432728312bf2
Pedro BM <p.bonilla.miralles@gmail.com>
2019-02-27 21:40:44 +0100

    First routes started

---
+++ routes/edit.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const Content = require('../models/content');
+const router = express.Router();
+
+router.get('/edit', (req, res, next) => {
+  
+})
---
+++ routes/home.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const Cv = require('../models/cv');
+
+const router = express.Router();
+
+router.get('/home', (req, res, next) => {
+  Cv.find({})
+    .then((cv) => {
+      res.render('/home', { cv, title: 'Iron-cv' })
+    })
+    .catch((error) => {
+      console.log('error');
+    })
+})
