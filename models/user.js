b2bfbde92086dcc7eaae1dab3b4d0ac323f4e439
Pedro BM <p.bonilla.miralles@gmail.com>
2019-04-08 20:39:50 +0200

    default avatar added

--- models/user.js
+++ models/user.js
@@ -26,7 +26,7 @@ const userSchema = new Schema({
     language: String,
     level: String,
   }],
-  avatarURL: String,
+  avatarURL: { type: String, default: 'https://firebasestorage.googleapis.com/v0/b/iron-cv.appspot.com/o/images%2F1dcf5ae8-eebd-4fc7-b84d-00669bd05142.jpeg?alt=media&token=2e9219a9-4204-4330-a6d0-3e63742c3350'}
 }, {
   timestamps: {
     createdAt: 'created_at',
